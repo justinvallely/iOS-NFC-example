a7f9498a6917e6ed2e47ba039c0c42955723e881
Justin Vallely <justin.vallely@ibotta.com>
2018-06-07 15:51:40 -0600

    Initial Commit

---
+++ NFC Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NFC Test
+//
+//  Created by Justin Vallely on 6/7/18.
+//  Copyright © 2018 Ibotta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
